424b4fd1a5f3ccf8907c493e784aad6b6279d61b
dariader <dary.spb@gmail.com>
2023-02-22 01:13:42 +0200

    fix

--- models/staging/stg_fhv_data.sql
+++ models/staging/stg_fhv_data.sql
@@ -9,32 +9,17 @@ with tripdata as
 )
 select
    -- identifiers
-    {{ dbt_utils.surrogate_key(['vendorid', 'tpep_pickup_datetime']) }} as tripid,
+    {{ dbt_utils.surrogate_key(['dispatching_base_num', 'pickup_datetime']) }} as tripid,
+    cast(ratecodeid as integer) as ratecodeid,
     cast(pulocationid as integer) as  pickup_locationid,
     cast(dolocationid as integer) as dropoff_locationid,
 
     -- timestamps
-    cast(pickup_datetime as timestamp) as pickup_datetime,
-    cast(dropOff_datetime as timestamp) as dropoff_datetime,
+    cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
 
     -- trip info
-    store_and_fwd_flag,
-    cast(passenger_count as integer) as passenger_count,
-    cast(trip_distance as numeric) as trip_distance,
-    -- yellow cabs are always street-hail
-    1 as trip_type,
-
-    -- payment info
-    cast(fare_amount as numeric) as fare_amount,
-    cast(extra as numeric) as extra,
-    cast(mta_tax as numeric) as mta_tax,
-    cast(tip_amount as numeric) as tip_amount,
-    cast(tolls_amount as numeric) as tolls_amount,
-    cast(0 as numeric) as ehail_fee,
-    cast(improvement_surcharge as numeric) as improvement_surcharge,
-    cast(total_amount as numeric) as total_amount,
-    cast(payment_type as integer) as payment_type,
-    {{ get_payment_type_description('payment_type') }} as payment_type_description,
-    cast(congestion_surcharge as numeric) as congestion_surcharge
+    SR_Flag,
+    Affiliated_base_number,
 from tripdata
 where rn = 1
